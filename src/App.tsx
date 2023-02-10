4567e95285fde5378898c7b1281125be1189414a
Jia-luen Yang <102781876+JialuenYang@users.noreply.github.com>
2023-02-10 15:43:34 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import Player from "./Player";
+
+function App() {
+	return (
+		<div className="App">
+			<Player videoId="kiW7VQTryAc" />
+		</div>
+	);
+}
+
+export default App;
